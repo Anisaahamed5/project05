5c2e7229ffcc8fe4a152277c0525496311b799fa
Anisa Ahamed <anisa.ahamed5@gmail.com>
2021-01-20 21:31:14 -0700

    Added routes and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,140 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-import logo from './logo.svg';
+import Login from './pages/login';
+import Registration from './pages/registration';
+import Main from './pages/main';
+import NewQuestion from './pages/new_question';
+import ViewQuestion from './pages/view_question';
+import AnswerQuestion from './pages/answer_question';
 
 import './App.css';
 
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+  state = { 
+    user: null
+  }
+
+  authenticatedRoutes() {
+    if(this.state.user == null) {
+      return <Route path="/">
+        <Redirect
+          to={{
+            pathname: "/login",
+          }}
+        />
+      </Route>
+    } else {
+      return <>
+        <Route path="/:category/new_question">
+          <NewQuestion />
+        </Route>
+        <Route path="/:category/:question_id/question">
+          <ViewQuestion />
+        </Route>
+        <Route path="/:category/:question_id/answer">
+          <AnswerQuestion />
+        </Route>
+        <Route path="/">
+          <Main />
+        </Route>
+      </>
+    }
   }
   
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    
-    return body;
-  };
-  
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    
-    this.setState({ responseToPost: body });
-  };
-  
-render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-      </div>
-    );
+  render() {
+    return <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Registration />
+          </Route>
+          { this.authenticatedRoutes() }
+        </Switch>
+    </Router>
   }
 }
+
+// import logo from './logo.svg';
+
+// import './App.css';
+
+// class App extends Component {
+//   state = {
+//     response: '',
+//     post: '',
+//     responseToPost: '',
+//   };
+  
+//   componentDidMount() {
+//     this.callApi()
+//       .then(res => this.setState({ response: res.express }))
+//       .catch(err => console.log(err));
+//   }
+  
+//   callApi = async () => {
+//     const response = await fetch('/api/hello');
+//     const body = await response.json();
+//     if (response.status !== 200) throw Error(body.message);
+    
+//     return body;
+//   };
+  
+//   handleSubmit = async e => {
+//     e.preventDefault();
+//     const response = await fetch('/api/world', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ post: this.state.post }),
+//     });
+//     const body = await response.text();
+    
+//     this.setState({ responseToPost: body });
+//   };
+  
+// render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//         <p>{this.state.response}</p>
+//         <form onSubmit={this.handleSubmit}>
+//           <p>
+//             <strong>Post to Server:</strong>
+//           </p>
+//           <input
+//             type="text"
+//             value={this.state.post}
+//             onChange={e => this.setState({ post: e.target.value })}
+//           />
+//           <button type="submit">Submit</button>
+//         </form>
+//         <p>{this.state.responseToPost}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ client/src/pages/registration/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+import './registration.css';
+
+class Registration extends Component {
+    state = {
+        username: "",
+        password: "",
+        password2: "",
+        errors: []
+    }
+
+    onChange(data) {
+        this.setState({...data, errors: []});
+    }
+
+    renderErrors() {
+        return <ul className="text-danger pl-3">
+            {
+                this.state.errors.map((error) => {
+                    return <li>{error}</li>
+                })
+            }
+        </ul>;
+    }
+
+    handleSubmit() {
+        let errors = [];
+
+        if(this.state.password != this.state.password2) {
+            errors.push("Passwords do not match")
+        }
+
+        // Other validity checks
+        //
+
+        this.setState({
+            errors
+        });
+
+        if(errors.length < 1) {
+            this.submit();
+        }
+    }
+
+    // Submit the data to the API
+    submit() {
+
+    }
+
+    render() {
+        return (
+            <div id="registration" className="container">
+                { this.renderErrors() }
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" id="username" placeholder="Enter username" value={this.state.username} onChange={(e) => this.onChange({username: e.target.value})}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password1">Password</label>
+                        <input type="password" className="form-control" id="password1" placeholder="Password" value={this.state.password} onChange={(e) => this.onChange({password: e.target.value})}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control" id="password2" placeholder="Repeat Password" value={this.state.password2} onChange={(e) => this.onChange({password2: e.target.value})}/>
+                    </div>
+                    <button type="button" className="btn btn-primary" onClick={() => this.handleSubmit()}>Register</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Registration;
