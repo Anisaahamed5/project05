b1f6a2f6f48eccfbd5fb215ce67ac144eaae08b3
Anisa Ahamed <anisa.ahamed5@gmail.com>
2021-01-22 00:21:24 -0700

    main feed work

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import { useParams } from "react-router-dom";
+
+import Loader from 'react-loader-spinner'
 
 class Main extends Component {
     state = {
+        questions: [],
+        loading: false
+    }
 
+    async componentDidMount() {
+        this.setState({loading: true});
+
+        let { category } = useParams();
+        let response = await fetch('/api/feed/' + category, {
+            method: 'GET'
+        });
     }
 
     render() {
-        return <>
-            <h1>Welcome to the Dashboard {this.props.user.username}!</h1>
-        </>;
+        return (
+            this.state.loading ? (
+                <div className="d-flex justify-content-center align-items-center h-100">
+                    <Loader
+                        type="TailSpin"
+                        color="#333"
+                        height={50}
+                        width={50}            
+                    />
+                </div>
+            ) : (
+                <h1>Loaded!</h1>
+            )
+        )
     }
 }
 
