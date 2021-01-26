bd9bbc01d04116a8d37c18379ac2dccbcc5fa765
Anisa Ahamed <anisa.ahamed5@gmail.com>
2021-01-26 14:24:22 -0700

    added new question button

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Loader from 'react-loader-spinner';
+import { Link } from 'react-router-dom';
 
 import Question from '../../components/question';
 
 import { withCategory, CATEGORIES } from '../../utils';
+
+import './main.css';
 
 class Main extends Component {
     state = {
@@ -33,12 +36,17 @@ class Main extends Component {
         this.setState({questions, loading: false})
     }
 
+    // map is only used on lists, questions is an []. 
+    // map takes in a function and applies it to every item in a list, then creates a new list with the results.
+
     renderQuestions() {
         return this.state.questions.map((q) => {
-            return <Question question={q} />
+            return <Question question={q} />;
         });
     }
 
+    // Ternary operator: expression ? true : false;
+    // The list of questions are rendered below
     render() {
         return (
             this.state.loading ? (
@@ -51,7 +59,12 @@ class Main extends Component {
                     />
                 </div>
             ) : (
-                this.renderQuestions()
+                <>
+                    <Link to={`/${this.props.category}/new_question`}>
+                        <button className="btn btn-primary new-q-button">New Question</button>
+                    </Link>
+                    { this.renderQuestions() }
+                </>
             )
         )
     }
